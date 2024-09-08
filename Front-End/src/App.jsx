2f2d9ad2fb6dd3b5ed622ca1491b731b564b8454
Shiraz Mazhar <imshiraz007@gmail.com>
2024-09-08 17:38:47 +0500

    Front-Page Banner

--- Front-End/src/App.jsx
+++ Front-End/src/App.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
-
+import HomeBanner from './components/HomeBanner'
+import SmallCard from './components/Card'
+import orderOnlineImg from '/images/orderonline.jpg'
+import diningoutImg from '/images/diningout.jpg'
 const App = () => {
   return (
-    <div className='font-bold text-lg'>
-    <p className='text-3xl font-bold underline'>Testing</p>
-    </div>
+    <>
+      <HomeBanner />
+      <div className='w-[70%] m-auto'>
+        <div className='flex flex-wrap items-center justify-center'>
+          <SmallCard
+            imgSrc={orderOnlineImg}
+            text={"Order Online"}
+            link={"orderonline"}
+            additionalText={"Stay home and order at your doorstep"}
+          />
+          <SmallCard
+            imgSrc={diningoutImg}
+            text={"Order Online"}
+            link={"Dining Out"}
+            additionalText={"Check out our best resturant that offers dine out"}
+          />
+        </div>
+      </div>
+    </>
   )
 }
 
-export default App;
+export default App
