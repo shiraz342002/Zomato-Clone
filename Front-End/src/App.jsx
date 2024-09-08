02f03ed895c97fb6ed792a970fe7463e303ef348
Shiraz Mazhar <imshiraz007@gmail.com>
2024-09-08 13:27:35 +0500

    Setup

---
+++ Front-End/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='font-bold text-lg'>
+    <p className='text-3xl font-bold underline'>Testing</p>
+    </div>
+  )
+}
+
+export default App;
